13ced056a142f542f9ccb320727e5f4df7cb7841
Abdullah Momoh <momohabdullah20@gmail.com>
2024-09-19 17:03:19 +0100

    updated index.js

--- index.js
+++ index.js
@@ -40,16 +40,49 @@ var user = {
   lastName: "Doe",
   age: 40,
   likedFruits: ["Mango", "Banana"],
-};
+}; // use curly braces
 
 //Arrays
 var fruits = ["Mango", "Banana"];
+var numbers = [40, 20, 22];
 
 const PI = 3.142;
 pi = 3242; //throws an error
 
-let x = 5;
-let y = 10;
-let div = x / y;
 
-//RUN COMMAND: nodde index.js
+//Mathematical operators
+let x = 10;
+let y = 10;
+let div = x / y; // division /
+let mult = x * y; // multiply *
+div = x + y; // addition +
+div = x - y; // subtraction -
+div = x % y; // modulus % (remainder) 5%2 = 1
+div = x++; // post increment +1
+div = x--; //post decrement -1
+
+//Comparison operators (mostly boolean values)
+x == y // equal to without type checking
+x > y // greater than
+x < y // less than
+x <= y //less than or equal to 
+x >= y // greater than or equal to
+10 === '10' //equal to but also check its type
+
+//Logical Operations
+isLoggedIn && isDeveloper // and operator, both conditions are true
+false || false // or operator, if any value is true then it returns true
+!isMember // not operator
+
+// Assignment Operators
+z = y + 3
+z += y // z = z + y
+z -= y // z = z - y
+z *= y // z = z * y
+z /= y // z = z / y
+z %= y // z = z % y  10%7   
+
+
+//RUN COMMAND: node index.js
+
+
