7d823c11e6418b2e0b403e736f63745762530b4d
Abdullah Momoh <momohabdullah20@gmail.com>
2024-10-01 17:22:10 +0100

    main commit

---
+++ 008/index.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("inputValue");
+const userInput = input.value;
+
+const tailsOutput = document.querySelector("#tails");
+const headsOutput = document.querySelector("#heads");
+const button = document.querySelector("#runButton");
+
+function flipCoin() {
+  return Math.round(Math.random());
+}
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("user input", userInput);
+  let flips = {
+    heads: 0,
+    tails: 0,
+  };
+
+  for (let i = 0; i < userInput; i++) {
+    const flippedValue = flipCoin();
+    if (flippedValue === 0) {
+      flips.tails += 1;
+    } else {
+      flips.heads += 1;
+    }
+  }
+
+  headsOutput.innerHTML = flips.heads;
+  tailsOutput.innerHTML = flips.tails;
+});
