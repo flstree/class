a783780aa4390e02e037ef7a580bf317a536905e
Abdullah Momoh <momohabdullah20@gmail.com>
2024-10-18 23:50:20 +0100

    updating todo app

--- 009/index.js
+++ 009/index.js
@@ -1,13 +1,32 @@
 const input = document.getElementById("todoListInput");
 const button = document.getElementById("todoListButton");
 const list = document.querySelector(".list");
+let arrayOfItems = [];
+let counter = 0;
 
 button.addEventListener("click", function (event) {
   event.preventDefault();
   const inputBox = input.value?.toLowerCase();
-  list.innerHTML += `<li>
-            <input type="checkbox" name="listItem" value="${inputBox}">
-            <label for="listItem">${inputBox}</label>
-            <span>X</span>
-        </li>`;
+  arrayOfItems.push(inputBox);
+
+  buildlist();
 });
+
+function deleteList(index) {
+  arrayOfItems.splice(index, 1);
+  buildlist();
+
+  //list.removeChild(listitem);
+}
+
+function buildlist() {
+  let buildList = "";
+  for (let i = 0; i < arrayOfItems.length; i++) {
+    buildList += `<li id='listItem${i}'>
+            <input type="checkbox" name="listItem" value="${arrayOfItems[i]}">
+            <label for="listItem">${arrayOfItems[i]}</label>
+            <span onclick='deleteList(${i})'>X</span>
+        </li>`;
+  }
+  list.innerHTML = buildList;
+}
